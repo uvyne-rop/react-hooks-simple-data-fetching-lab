5da60667ba77ef4c0f5a0a678c7da06ac6ccb632
uvyne-rop <ropchiri7@gmail.com>
2024-04-29 13:20:53 +0300

    initial commits

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+// // create your App component here
+
+import React , { useState, useEffect } from "react";
+
+function App() {
+    const [dogImage, setDogImage] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setDogImage(data.message);
+            setLoading(false);
+
+        })
+    }, []);
+    return (
+        <div>
+            {loading ? <p>Loading</p> : <img src={dogImage} alt="A Random Dog"/>}
+        </div>
+    );
+}
+export default App;
+
